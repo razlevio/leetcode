b8375bc9aed44213905bfc25f26e6cf84e820b3b
Raz Levi <66571396+razlevio@users.noreply.github.com>
2022-02-11 10:47:39 +0200

    Create BuySellStockII.java

---
+++ Algorithms/Java/Best-Time-to-Buy-and-Sell-Stock-II/BuySellStockII.java
@@ -0,0 +1,16 @@
+class BuySellStockII {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < min) {
+                min = prices[i];
+            } else if(prices[i] > min) {
+                maxProfit += prices[i] - min;
+                min = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
